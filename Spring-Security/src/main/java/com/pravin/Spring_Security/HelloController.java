b3e2a3cdd2d529e34e9729808e72bdc13cbfd820
PravinLengare <lengarepravin115@gmail.com>
2025-10-16 16:01:34 +0530

    till csrf token disable

--- Spring-Security/src/main/java/com/pravin/Spring_Security/HelloController.java
+++ Spring-Security/src/main/java/com/pravin/Spring_Security/HelloController.java
@@ -1,13 +1,21 @@
 package com.pravin.Spring_Security;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpRequest;
 
 @RestController
 public class HelloController {
 
     @GetMapping("hello")
-    public String greet(){
-        return "Hello World";
+    public String greet(HttpServletRequest request){
+        return "Hello World "+request.getSession().getId();
+    }
+
+    @GetMapping("about")
+    public String about(HttpServletRequest request){
+        return "Pravin Lengare "+request.getSession().getId();
     }
 }
---
+++ Spring-Security/src/main/java/com/pravin/Spring_Security/Student.java
@@ -0,0 +1,15 @@
+package com.pravin.Spring_Security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+    private String tech;
+
+}
