90e9be6660d5cbc3b018c0e5abba18c41ccb3b2b
PravinLengare <lengarepravin115@gmail.com>
2025-11-02 19:29:48 +0530

    added memory to llm

--- Spring-AI/src/main/java/com/pravin/Spring_AI/OpenAIController.java
+++ Spring-AI/src/main/java/com/pravin/Spring_AI/OpenAIController.java
@@ -1,9 +1,12 @@
 package com.pravin.Spring_AI;
 
 import org.springframework.ai.chat.client.ChatClient;
-
-import org.springframework.ai.chat.messages.Message;
-import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,7 +33,7 @@ public class OpenAIController {
      */
 
     // using the chat Client
-
+    /*
     private ChatClient chatClient;
 
     public OpenAIController(OpenAiChatModel chatModel) {
@@ -43,6 +46,43 @@ public class OpenAIController {
                             .prompt(message)
                             .call()
                             .content();
+
+        return ResponseEntity.ok(response);
+    }
+
+     */
+
+    // using metadata
+
+    private ChatClient chatClient;
+
+//    public OpenAIController(OpenAiChatModel chatModel) {
+//        this.chatClient = ChatClient.create(chatModel);
+//    }
+
+    ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+
+    public OpenAIController(ChatClient.Builder builder){
+        this.chatClient = builder
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
+
+    }
+
+    @GetMapping("/api/{message}")
+    public ResponseEntity<String> getResponse(@PathVariable String message){
+
+       ChatResponse chatResponse = chatClient
+                .prompt(message)
+                .call()
+                .chatResponse();
+
+        System.out.println(chatResponse.getMetadata().getModel());
+
+       String response = chatResponse
+               .getResult()
+               .getOutput()
+               .getText();
 
         return ResponseEntity.ok(response);
     }
