7d89f05cb5df8c00c4a12aae8837a4353547dd89
PravinLengare <lengarepravin115@gmail.com>
2025-11-04 07:20:42 +0530

    using the ollama and templatae

---
+++ Spring-AI/src/main/java/com/pravin/Spring_AI/OllamaController.java
@@ -0,0 +1,51 @@
+package com.pravin.Spring_AI;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.ollama.OllamaChatModel;
+
+@RestController
+public class OllamaController {
+
+    private ChatClient chatClient;
+
+    public OllamaController(OllamaChatModel chatModel) {
+        this.chatClient = ChatClient.create(chatModel);
+    }
+
+   //  ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+
+//    public OpenAIController(ChatClient.Builder builder){
+//        this.chatClient = builder
+//                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+//                .build();
+//
+//    }
+
+    //@GetMapping("/api/{message}")
+    public ResponseEntity<String> getResponse(@PathVariable String message){
+
+        ChatResponse chatResponse = chatClient
+                .prompt(message)
+                .call()
+                .chatResponse();
+
+        System.out.println(chatResponse.getMetadata().getModel());
+
+        String response = chatResponse
+                .getResult()
+                .getOutput()
+                .getText();
+
+        return ResponseEntity.ok(response);
+    }
+
+
+}
--- Spring-AI/src/main/java/com/pravin/Spring_AI/OpenAIController.java
+++ Spring-AI/src/main/java/com/pravin/Spring_AI/OpenAIController.java
@@ -2,15 +2,17 @@ package com.pravin.Spring_AI;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class OpenAIController {
@@ -22,6 +24,9 @@ public class OpenAIController {
     public OpenAIController(OpenAiChatModel chatModel) {
         this.chatModel = chatModel;
     }
+
+    // constructor injection
+
     @GetMapping("/api/{message}")
     public String getResponse(@PathVariable String message){
 
@@ -56,18 +61,20 @@ public class OpenAIController {
 
     private ChatClient chatClient;
 
-//    public OpenAIController(OpenAiChatModel chatModel) {
-//        this.chatClient = ChatClient.create(chatModel);
+    public OpenAIController(OpenAiChatModel chatModel) {
+        this.chatClient = ChatClient.create(chatModel);
+    }
+
+//    ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+//
+//
+//    public OpenAIController(@Qualifier("openAiChatModel") ChatClient.Builder builder){
+//        this.chatClient = builder
+//                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+//                .build();
+//
 //    }
 
-    ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
-
-    public OpenAIController(ChatClient.Builder builder){
-        this.chatClient = builder
-                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
-                .build();
-
-    }
 
     @GetMapping("/api/{message}")
     public ResponseEntity<String> getResponse(@PathVariable String message){
@@ -85,5 +92,36 @@ public class OpenAIController {
                .getText();
 
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/api/recommend")
+    public String recommend(@RequestParam String type , @RequestParam String year, @RequestParam String lang){
+
+        String tempt = """
+                                I want to watch a {type} movie tonight with good rating,
+                                looking  for movies around this year {year}.
+                                The  language im looking for is {lang}.
+                                Suggest one specific movie and tell me the cast and length of the movie.
+                                
+                                response format should be:
+                                        1. Movie Name
+                                        2. basic plot
+                                        3. cast
+                                        4. length
+                                        5. IMDB rating
+                        """;
+
+        PromptTemplate promptTemplate = new PromptTemplate(tempt);
+        Prompt prompt = promptTemplate.create(Map.of(
+                                                    "type",type,
+                                                    "year",year,
+                                                    "lang",lang));
+
+        String response = chatClient
+                .prompt(prompt)
+                .call()
+                .content();
+
+        return response;
     }
 }
